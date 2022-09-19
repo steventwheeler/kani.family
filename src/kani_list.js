cc78458b8299f5d2d104a284d44b2373e59a73b7
Steven Wheeler <swheeler@usinternet.com>
2022-09-19 16:58:07 -0500

    Fix a typo

--- src/kani_list.js
+++ src/kani_list.js
@@ -13,7 +13,7 @@ function intWithSign(i) {
 }
 
 function attributeRankSpan(value, rank) {
-  var buffOrBuffs = "buff" + Math.abs(rank) == 1?"":"s"
+  var buffOrBuffs = "buff" + (Math.abs(rank) == 1?"":"s");
   var title = "This attribute is " + (rank == 0?"equal to":(Math.abs(rank) + " " + buffOrBuffs + " " + (rank < 0?"below":"above"))) + " the median value.";
   return "<span class='attribute' title='" + title + "'>" + value + "<sup class='rank'>" + intWithSign(rank) + "</sup></span>"
 }

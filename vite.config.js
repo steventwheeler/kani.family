6c95f42149211976af43cbef711158bba3be8887
Steven Wheeler <swheeler@usinternet.com>
2023-01-24 23:08:36 -0600

    Fix dist files

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       include: []
     },
     rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+      // plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
     },
   },
   define: {
